807aa2f614a51c8cc3eae3cbcd50b1f1b9f7a4f7
Stewart Kwok <stewart.kwok1@gmail.com>
2023-08-25 16:03:21 -0700

    Fix sidebar formatting

--- sidebar.py
+++ sidebar.py
@@ -11,9 +11,13 @@ Y_BUFFER_BTWN_ICONS = (screen.HEIGHT - (entity.ICON_DIMS[1] * len(entity.imgs)))
 def render(window):
     index = 0
     pygame.draw.rect(window, screen.LIGHT_GRAY, (screen.WIDTH, 0, screen.SIDEBAR_WIDTH, screen.HEIGHT))
+    pygame.draw.rect(window, screen.BLACK, (screen.WIDTH, 0, 3, screen.HEIGHT))
     for icon in entity.icons:
         window.blit(icon, (screen.WIDTH + (screen.SIDEBAR_WIDTH-entity.ICON_DIMS[0])/2, Y_BUFFER_BTWN_ICONS * (index+1) + entity.ICON_DIMS[1] * index))
-        renderText(f"{entityManager.counts[index]}", font, screen.WHITE, screen.WIDTH + (screen.SIDEBAR_WIDTH-entity.ICON_DIMS[0])/2 + 50, Y_BUFFER_BTWN_ICONS * (index+1) + entity.ICON_DIMS[1] * index + 50, window)
+        text = f"{entityManager.counts[index]}"
+        text_size = font.size(text)
+        text_buffer_y = (screen.HEIGHT - (text_size[1] * len(entity.imgs))) / (len(entity.imgs) + 1)
+        renderText(text, font, screen.WHITE, screen.WIDTH + (screen.SIDEBAR_WIDTH-text_size[0])/2, text_buffer_y * (index+1) + text_size[1] * index, window)
         index += 1
 def renderText(text, font, text_col, x, y, window):
     img = font.render(text, True, text_col)
